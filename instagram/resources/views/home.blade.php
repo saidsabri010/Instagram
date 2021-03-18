fee80ebd6a572046bd23ed54e71bbbe23e4680a7
said <js2907846@gmail.com>
2021-03-18 01:47:27 +0100

    Designing UI

--- instagram/resources/views/home.blade.php
+++ instagram/resources/views/home.blade.php
@@ -2,21 +2,38 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-3">
+          <img  class="img-fluid" src="http://techfameplus.com/wp-content/uploads/2015/01/Spartan-Logo-design-By-Haider-Ali.png" class="img-rounded">
+        </div>
+        <div class="col-9">
+            <div>
+                <h2> <b>Computer Science</b> </h2>
             </div>
+            <div class="d-flex">
+                <div class="pl-5" > <strong>10K</strong> Posts </div>
+                <div class="pl-5" ><strong>7K</strong> Followers</div>
+                <div class="pl-5" ><strong>500</strong> Following</div>
+            </div>
+            <div class="font-weight-bold pt-4" ><p>ComputerScience.ogr</p></div>
+            <div>
+                Computer Science is the study of computers and computational systems. 
+                    Unlike electrical and computer engineers, computer scientists deal 
+                    mostly with software and software systems;
+                    this includes their theory, design, development, and application.
+            </div>
+            <div><a href="#">ComputerScience.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-4 ">
+        <div class="col-3" >
+            <img  src="https://images.pexels.com/photos/48753/pexels-photo-48753.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style="width:100%">
+        </div>
+        <div class="col-3" >
+            <img src="https://images.pexels.com/photos/7057/desk-office-computer-imac.jpg?auto=compress&cs=tinysrgb&h=750&w=1260" style="width:100%">
+        </div>
+        <div class="col-3 img-responsive pull-right">
+            <img src="https://images.pexels.com/photos/7374/startup-photos.jpg?auto=compress&cs=tinysrgb&h=750&w=1260" style="width:100%">
         </div>
     </div>
 </div>
