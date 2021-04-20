42879e09d478b90d629cfa37267c2b5a58df7be1
said <js2907846@gmail.com>
2021-04-21 00:56:44 +0200

    Passing Data to the View

--- instagram/routes/web.php
+++ instagram/routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'ProfileController@index')->name('home');
+Route::get('/home/{user}', 'ProfileController@index')->name('home.show');
